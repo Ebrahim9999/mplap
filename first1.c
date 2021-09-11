be302ce9a7bbc0421f3e4781fa4b15c4c72e1f8e
Ebrahim9999 <ebraelgn19@gmail.com>
2021-09-11 16:32:17 +0200

    first one

---
+++ first1.c
@@ -0,0 +1,15 @@
+/*
+ * File:   first1.c
+ * Author: M E T R O
+ *
+ * Created on 11 ??????, 2021, 04:17 ?
+ */
+
+
+#include <avr/io.h>
+
+int main(void) {
+    /* Replace with your application code */
+    while (1) {
+    }
+}
